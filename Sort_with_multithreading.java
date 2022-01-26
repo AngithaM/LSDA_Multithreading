e7eca62f096a184162acee2cfea74aeff5f7fa7a
AngithaM <74540513+AngithaM@users.noreply.github.com>
2022-01-26 16:59:16 +0000

    Update and rename Sort_For_Q3_Ans_2.java to Sort_with_multithreading.java

--- Sort_with_multithreading.java
+++ Sort_with_multithreading.java
@@ -1,25 +1,10 @@
-/**
- * 
- * Modify either your code for any of the previous questions or the code provided on this assignment sheet so that it makes 
-sensible use of parallel computing (using multithreading) in the body of method sort(int low, int n). 
-* Do not change the algorithm otherwise and do not use any other sorting routine (such as one of Java’s built-in sorting 
-approaches). 
-* Do not change any code outside the body of method sort(int low, int n).
-* Use only the "traditional" (Java <= 7) approach to multithreading for this question (using class Thread explicitly); do not use, e.g.,
-parallel Java 8 Streams.
-* It is not required that your parallelized code runs actually faster than the original code (no need to run any benchmarks) - but it 
-should make use of concurrent threads in a sensible way.
-[50 Marks]
- */
-/**
-ANSWER 2: Using multiple threads to run sort of Array of Strings in such a way that the merge is done in parallel within the splits
-**/
+
 package LSDA;
 
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Sort_For_Q3_Ans_2 extends Thread {
+public class Sort_with_multithreading extends Thread {
 	public String[] array;
 	public Comparator<String> comp;
 	int low;
@@ -30,7 +15,7 @@ public class Sort_For_Q3_Ans_2 extends Thread {
 
 		// A comparator for objects of type String:
 		Comparator<String> compString = (String a, String b) -> a.compareTo(b);
-		Sort_For_Q3_Ans_2 sortStrings = new Sort_For_Q3_Ans_2();
+		Sort_with_multithreading sortStrings = new Sort_with_multithreading();
 
 		String[] arrayOfStrings = { "Blue", "Yellow", "Almond", "Onyx", "Peach", "Gold", "Red", "Melon", "Lava",
 				"Beige", "Aqua", "Lilac", "Capri", "Orange", "Mauve", "Plum" };
@@ -45,7 +30,7 @@ public class Sort_For_Q3_Ans_2 extends Thread {
 		this.array = array;
 		this.comp = comp;
 		try {
-			Sort_For_Q3_Ans_2 FirstThread = new Sort_For_Q3_Ans_2(array, comp, 0, array.length);// Calling sort
+			Sort_with_multithreading FirstThread = new Sort_with_multithreading(array, comp, 0, array.length);// Calling sort
 			FirstThread.start();
 			FirstThread.join();
 		} catch (InterruptedException e) {
@@ -53,26 +38,26 @@ public class Sort_For_Q3_Ans_2 extends Thread {
 		}
 	}
 
-	public Sort_For_Q3_Ans_2(String[] arr, Comparator<String> comp, int low, int n) {// Constructor to pass the values needed
+	public Sort_with_multithreading(String[] arr, Comparator<String> comp, int low, int n) {// Constructor to pass the values needed
 																				// for the thread
 		this.array = arr;
 		this.comp = comp;
 		this.low = low;
 		this.n = n;
-		System.out.println("Thread" + Sort_For_Q3_Ans_2.currentThread() + "  Intermediate Array" + Arrays.toString(arr));
+		System.out.println("Thread" + Sort_with_multithreading.currentThread() + "  Intermediate Array" + Arrays.toString(arr));
 		// partially sorted using multi-threading. Both halves are sorted in  parallel
 	}
 
-	public Sort_For_Q3_Ans_2() {
+	public Sort_with_multithreading() {
 		// Constructor for sortStrings object
 	}
 
 	public void run() {// enclosing the logic of the sort method in the run()
 		if (n > 1) {
 			int mid = n >> 1;
-			Sort_For_Q3_Ans_2 firstHalf = new Sort_For_Q3_Ans_2(array, comp, low, mid);// recursively calling sort method to split
+			Sort_with_multithreading firstHalf = new Sort_with_multithreading(array, comp, low, mid);// recursively calling sort method to split
 																			// and sort the first half
-			Sort_For_Q3_Ans_2 nextHalf = new Sort_For_Q3_Ans_2(array, comp, low + mid, mid);// recursively calling sort method to
+			Sort_with_multithreading nextHalf = new Sort_with_multithreading(array, comp, low + mid, mid);// recursively calling sort method to
 																				// split and sort the rest
 
 			try {
